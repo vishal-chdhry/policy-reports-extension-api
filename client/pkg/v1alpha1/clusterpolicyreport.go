781318113d349f625106c0c39500095b58eaa761
Vishal Choudhary <vishal.choudhary@nirmata.com>
2023-11-06 18:36:20 +0530

    feat: remove api extensions external
    Signed-off-by: Vishal Choudhary <vishal.choudhary@nirmata.com>

--- client/pkg/v1alpha1/clusterpolicyreport.go
+++ client/pkg/v1alpha1/clusterpolicyreport.go
@@ -4,8 +4,6 @@ import (
 	"context"
 	"time"
 
-	v1alpha2 "github.com/kyverno/kyverno/api/policyreport/v1alpha2"
-	scheme "github.com/kyverno/kyverno/pkg/client/clientset/versioned/scheme"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	types "k8s.io/apimachinery/pkg/types"
 	watch "k8s.io/apimachinery/pkg/watch"
@@ -20,14 +18,14 @@ type ClusterPolicyReportsGetter interface {
 
 // ClusterPolicyReportInterface has methods to work with ClusterPolicyReport resources.
 type ClusterPolicyReportInterface interface {
-	Create(ctx context.Context, clusterPolicyReport *v1alpha2.ClusterPolicyReport, opts v1.CreateOptions) (*v1alpha2.ClusterPolicyReport, error)
-	Update(ctx context.Context, clusterPolicyReport *v1alpha2.ClusterPolicyReport, opts v1.UpdateOptions) (*v1alpha2.ClusterPolicyReport, error)
+	Create(ctx context.Context, clusterPolicyReport *ClusterPolicyReport, opts v1.CreateOptions) (*ClusterPolicyReport, error)
+	Update(ctx context.Context, clusterPolicyReport *ClusterPolicyReport, opts v1.UpdateOptions) (*ClusterPolicyReport, error)
 	Delete(ctx context.Context, name string, opts v1.DeleteOptions) error
 	DeleteCollection(ctx context.Context, opts v1.DeleteOptions, listOpts v1.ListOptions) error
-	Get(ctx context.Context, name string, opts v1.GetOptions) (*v1alpha2.ClusterPolicyReport, error)
-	List(ctx context.Context, opts v1.ListOptions) (*v1alpha2.ClusterPolicyReportList, error)
+	Get(ctx context.Context, name string, opts v1.GetOptions) (*ClusterPolicyReport, error)
+	List(ctx context.Context, opts v1.ListOptions) (*ClusterPolicyReportList, error)
 	Watch(ctx context.Context, opts v1.ListOptions) (watch.Interface, error)
-	Patch(ctx context.Context, name string, pt types.PatchType, data []byte, opts v1.PatchOptions, subresources ...string) (result *v1alpha2.ClusterPolicyReport, err error)
+	Patch(ctx context.Context, name string, pt types.PatchType, data []byte, opts v1.PatchOptions, subresources ...string) (result *ClusterPolicyReport, err error)
 }
 
 // clusterPolicyReports implements ClusterPolicyReportInterface
@@ -43,27 +41,27 @@ func newClusterPolicyReports(c *DemoPolicyV1alpha2Client) *clusterPolicyReports 
 }
 
 // Get takes name of the clusterPolicyReport, and returns the corresponding clusterPolicyReport object, and an error if there is any.
-func (c *clusterPolicyReports) Get(ctx context.Context, name string, opts v1.GetOptions) (result *v1alpha2.ClusterPolicyReport, err error) {
-	result = &v1alpha2.ClusterPolicyReport{}
+func (c *clusterPolicyReports) Get(ctx context.Context, name string, opts v1.GetOptions) (result *ClusterPolicyReport, err error) {
+	result = &ClusterPolicyReport{}
 	err = c.client.Get().
 		Resource("clusterpolicyreports").
 		Name(name).
-		SpecificallyVersionedParams(&opts, scheme.ParameterCodec, GroupVersion).
+		SpecificallyVersionedParams(&opts, ParameterCodec, GroupVersion).
 		Do(ctx).
 		Into(result)
 	return
 }
 
 // List takes label and field selectors, and returns the list of ClusterPolicyReports that match those selectors.
-func (c *clusterPolicyReports) List(ctx context.Context, opts v1.ListOptions) (result *v1alpha2.ClusterPolicyReportList, err error) {
+func (c *clusterPolicyReports) List(ctx context.Context, opts v1.ListOptions) (result *ClusterPolicyReportList, err error) {
 	var timeout time.Duration
 	if opts.TimeoutSeconds != nil {
 		timeout = time.Duration(*opts.TimeoutSeconds) * time.Second
 	}
-	result = &v1alpha2.ClusterPolicyReportList{}
+	result = &ClusterPolicyReportList{}
 	err = c.client.Get().
 		Resource("clusterpolicyreports").
-		SpecificallyVersionedParams(&opts, scheme.ParameterCodec, GroupVersion).
+		SpecificallyVersionedParams(&opts, ParameterCodec, GroupVersion).
 		Timeout(timeout).
 		Do(ctx).
 		Into(result)
@@ -79,17 +77,17 @@ func (c *clusterPolicyReports) Watch(ctx context.Context, opts v1.ListOptions) (
 	opts.Watch = true
 	return c.client.Get().
 		Resource("clusterpolicyreports").
-		SpecificallyVersionedParams(&opts, scheme.ParameterCodec, GroupVersion).
+		SpecificallyVersionedParams(&opts, ParameterCodec, GroupVersion).
 		Timeout(timeout).
 		Watch(ctx)
 }
 
 // Create takes the representation of a clusterPolicyReport and creates it.  Returns the server's representation of the clusterPolicyReport, and an error, if there is any.
-func (c *clusterPolicyReports) Create(ctx context.Context, clusterPolicyReport *v1alpha2.ClusterPolicyReport, opts v1.CreateOptions) (result *v1alpha2.ClusterPolicyReport, err error) {
-	result = &v1alpha2.ClusterPolicyReport{}
+func (c *clusterPolicyReports) Create(ctx context.Context, clusterPolicyReport *ClusterPolicyReport, opts v1.CreateOptions) (result *ClusterPolicyReport, err error) {
+	result = &ClusterPolicyReport{}
 	err = c.client.Post().
 		Resource("clusterpolicyreports").
-		SpecificallyVersionedParams(&opts, scheme.ParameterCodec, GroupVersion).
+		SpecificallyVersionedParams(&opts, ParameterCodec, GroupVersion).
 		Body(clusterPolicyReport).
 		Do(ctx).
 		Into(result)
@@ -97,12 +95,12 @@ func (c *clusterPolicyReports) Create(ctx context.Context, clusterPolicyReport *
 }
 
 // Update takes the representation of a clusterPolicyReport and updates it. Returns the server's representation of the clusterPolicyReport, and an error, if there is any.
-func (c *clusterPolicyReports) Update(ctx context.Context, clusterPolicyReport *v1alpha2.ClusterPolicyReport, opts v1.UpdateOptions) (result *v1alpha2.ClusterPolicyReport, err error) {
-	result = &v1alpha2.ClusterPolicyReport{}
+func (c *clusterPolicyReports) Update(ctx context.Context, clusterPolicyReport *ClusterPolicyReport, opts v1.UpdateOptions) (result *ClusterPolicyReport, err error) {
+	result = &ClusterPolicyReport{}
 	err = c.client.Put().
 		Resource("clusterpolicyreports").
 		Name(clusterPolicyReport.Name).
-		SpecificallyVersionedParams(&opts, scheme.ParameterCodec, GroupVersion).
+		SpecificallyVersionedParams(&opts, ParameterCodec, GroupVersion).
 		Body(clusterPolicyReport).
 		Do(ctx).
 		Into(result)
@@ -113,7 +111,7 @@ func (c *clusterPolicyReports) Update(ctx context.Context, clusterPolicyReport *
 func (c *clusterPolicyReports) Delete(ctx context.Context, name string, opts v1.DeleteOptions) error {
 	return c.client.Delete().
 		Resource("clusterpolicyreports").
-		SpecificallyVersionedParams(&opts, scheme.ParameterCodec, GroupVersion).
+		SpecificallyVersionedParams(&opts, ParameterCodec, GroupVersion).
 		Name(name).
 		Body(&opts).
 		Do(ctx).
@@ -128,7 +126,7 @@ func (c *clusterPolicyReports) DeleteCollection(ctx context.Context, opts v1.Del
 	}
 	return c.client.Delete().
 		Resource("clusterpolicyreports").
-		SpecificallyVersionedParams(&listOpts, scheme.ParameterCodec, GroupVersion).
+		SpecificallyVersionedParams(&listOpts, ParameterCodec, GroupVersion).
 		Timeout(timeout).
 		Body(&opts).
 		Do(ctx).
@@ -136,13 +134,13 @@ func (c *clusterPolicyReports) DeleteCollection(ctx context.Context, opts v1.Del
 }
 
 // Patch applies the patch and returns the patched clusterPolicyReport.
-func (c *clusterPolicyReports) Patch(ctx context.Context, name string, pt types.PatchType, data []byte, opts v1.PatchOptions, subresources ...string) (result *v1alpha2.ClusterPolicyReport, err error) {
-	result = &v1alpha2.ClusterPolicyReport{}
+func (c *clusterPolicyReports) Patch(ctx context.Context, name string, pt types.PatchType, data []byte, opts v1.PatchOptions, subresources ...string) (result *ClusterPolicyReport, err error) {
+	result = &ClusterPolicyReport{}
 	err = c.client.Patch(pt).
 		Resource("clusterpolicyreports").
 		Name(name).
 		SubResource(subresources...).
-		SpecificallyVersionedParams(&opts, scheme.ParameterCodec, GroupVersion).
+		SpecificallyVersionedParams(&opts, ParameterCodec, GroupVersion).
 		Body(data).
 		Do(ctx).
 		Into(result)
--- client/pkg/v1alpha1/policyreport.go
+++ client/pkg/v1alpha1/policyreport.go
@@ -4,9 +4,8 @@ import (
 	"context"
 	"time"
 
-	v1alpha2 "github.com/kyverno/kyverno/api/policyreport/v1alpha2"
-	scheme "github.com/kyverno/kyverno/pkg/client/clientset/versioned/scheme"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 	types "k8s.io/apimachinery/pkg/types"
 	watch "k8s.io/apimachinery/pkg/watch"
 	rest "k8s.io/client-go/rest"
@@ -18,16 +17,19 @@ type PolicyReportsGetter interface {
 	PolicyReports(namespace string) PolicyReportInterface
 }
 
+var Scheme = runtime.NewScheme()
+var ParameterCodec = runtime.NewParameterCodec(Scheme)
+
 // PolicyReportInterface has methods to work with PolicyReport resources.
 type PolicyReportInterface interface {
-	Create(ctx context.Context, policyReport *v1alpha2.PolicyReport, opts v1.CreateOptions) (*v1alpha2.PolicyReport, error)
-	Update(ctx context.Context, policyReport *v1alpha2.PolicyReport, opts v1.UpdateOptions) (*v1alpha2.PolicyReport, error)
+	Create(ctx context.Context, policyReport *PolicyReport, opts v1.CreateOptions) (*PolicyReport, error)
+	Update(ctx context.Context, policyReport *PolicyReport, opts v1.UpdateOptions) (*PolicyReport, error)
 	Delete(ctx context.Context, name string, opts v1.DeleteOptions) error
 	DeleteCollection(ctx context.Context, opts v1.DeleteOptions, listOpts v1.ListOptions) error
-	Get(ctx context.Context, name string, opts v1.GetOptions) (*v1alpha2.PolicyReport, error)
-	List(ctx context.Context, opts v1.ListOptions) (*v1alpha2.PolicyReportList, error)
+	Get(ctx context.Context, name string, opts v1.GetOptions) (*PolicyReport, error)
+	List(ctx context.Context, opts v1.ListOptions) (*PolicyReportList, error)
 	Watch(ctx context.Context, opts v1.ListOptions) (watch.Interface, error)
-	Patch(ctx context.Context, name string, pt types.PatchType, data []byte, opts v1.PatchOptions, subresources ...string) (result *v1alpha2.PolicyReport, err error)
+	Patch(ctx context.Context, name string, pt types.PatchType, data []byte, opts v1.PatchOptions, subresources ...string) (result *PolicyReport, err error)
 }
 
 // policyReports implements PolicyReportInterface
@@ -45,29 +47,29 @@ func newPolicyReports(c *DemoPolicyV1alpha2Client, namespace string) *policyRepo
 }
 
 // Get takes name of the policyReport, and returns the corresponding policyReport object, and an error if there is any.
-func (c *policyReports) Get(ctx context.Context, name string, options v1.GetOptions) (result *v1alpha2.PolicyReport, err error) {
-	result = &v1alpha2.PolicyReport{}
+func (c *policyReports) Get(ctx context.Context, name string, options v1.GetOptions) (result *PolicyReport, err error) {
+	result = &PolicyReport{}
 	err = c.client.Get().
 		Namespace(c.ns).
 		Resource("policyreports").
 		Name(name).
-		SpecificallyVersionedParams(&options, scheme.ParameterCodec, GroupVersion).
+		SpecificallyVersionedParams(&options, ParameterCodec, GroupVersion).
 		Do(ctx).
 		Into(result)
 	return
 }
 
 // List takes label and field selectors, and returns the list of PolicyReports that match those selectors.
-func (c *policyReports) List(ctx context.Context, opts v1.ListOptions) (result *v1alpha2.PolicyReportList, err error) {
+func (c *policyReports) List(ctx context.Context, opts v1.ListOptions) (result *PolicyReportList, err error) {
 	var timeout time.Duration
 	if opts.TimeoutSeconds != nil {
 		timeout = time.Duration(*opts.TimeoutSeconds) * time.Second
 	}
-	result = &v1alpha2.PolicyReportList{}
+	result = &PolicyReportList{}
 	err = c.client.Get().
 		Namespace(c.ns).
 		Resource("policyreports").
-		SpecificallyVersionedParams(&opts, scheme.ParameterCodec, GroupVersion).
+		SpecificallyVersionedParams(&opts, ParameterCodec, GroupVersion).
 		Timeout(timeout).
 		Do(ctx).
 		Into(result)
@@ -84,18 +86,18 @@ func (c *policyReports) Watch(ctx context.Context, opts v1.ListOptions) (watch.I
 	return c.client.Get().
 		Namespace(c.ns).
 		Resource("policyreports").
-		SpecificallyVersionedParams(&opts, scheme.ParameterCodec, GroupVersion).
+		SpecificallyVersionedParams(&opts, ParameterCodec, GroupVersion).
 		Timeout(timeout).
 		Watch(ctx)
 }
 
 // Create takes the representation of a policyReport and creates it.  Returns the server's representation of the policyReport, and an error, if there is any.
-func (c *policyReports) Create(ctx context.Context, policyReport *v1alpha2.PolicyReport, opts v1.CreateOptions) (result *v1alpha2.PolicyReport, err error) {
-	result = &v1alpha2.PolicyReport{}
+func (c *policyReports) Create(ctx context.Context, policyReport *PolicyReport, opts v1.CreateOptions) (result *PolicyReport, err error) {
+	result = &PolicyReport{}
 	err = c.client.Post().
 		Namespace(c.ns).
 		Resource("policyreports").
-		SpecificallyVersionedParams(&opts, scheme.ParameterCodec, GroupVersion).
+		SpecificallyVersionedParams(&opts, ParameterCodec, GroupVersion).
 		Body(policyReport).
 		Do(ctx).
 		Into(result)
@@ -103,13 +105,13 @@ func (c *policyReports) Create(ctx context.Context, policyReport *v1alpha2.Polic
 }
 
 // Update takes the representation of a policyReport and updates it. Returns the server's representation of the policyReport, and an error, if there is any.
-func (c *policyReports) Update(ctx context.Context, policyReport *v1alpha2.PolicyReport, opts v1.UpdateOptions) (result *v1alpha2.PolicyReport, err error) {
-	result = &v1alpha2.PolicyReport{}
+func (c *policyReports) Update(ctx context.Context, policyReport *PolicyReport, opts v1.UpdateOptions) (result *PolicyReport, err error) {
+	result = &PolicyReport{}
 	err = c.client.Put().
 		Namespace(c.ns).
 		Resource("policyreports").
 		Name(policyReport.Name).
-		SpecificallyVersionedParams(&opts, scheme.ParameterCodec, GroupVersion).
+		SpecificallyVersionedParams(&opts, ParameterCodec, GroupVersion).
 		Body(policyReport).
 		Do(ctx).
 		Into(result)
@@ -122,7 +124,7 @@ func (c *policyReports) Delete(ctx context.Context, name string, opts v1.DeleteO
 		Namespace(c.ns).
 		Resource("policyreports").
 		Name(name).
-		SpecificallyVersionedParams(&opts, scheme.ParameterCodec, GroupVersion).
+		SpecificallyVersionedParams(&opts, ParameterCodec, GroupVersion).
 		Body(&opts).
 		Do(ctx).
 		Error()
@@ -137,7 +139,7 @@ func (c *policyReports) DeleteCollection(ctx context.Context, opts v1.DeleteOpti
 	return c.client.Delete().
 		Namespace(c.ns).
 		Resource("policyreports").
-		SpecificallyVersionedParams(&listOpts, scheme.ParameterCodec, GroupVersion).
+		SpecificallyVersionedParams(&listOpts, ParameterCodec, GroupVersion).
 		Timeout(timeout).
 		Body(&opts).
 		Do(ctx).
@@ -145,14 +147,14 @@ func (c *policyReports) DeleteCollection(ctx context.Context, opts v1.DeleteOpti
 }
 
 // Patch applies the patch and returns the patched policyReport.
-func (c *policyReports) Patch(ctx context.Context, name string, pt types.PatchType, data []byte, opts v1.PatchOptions, subresources ...string) (result *v1alpha2.PolicyReport, err error) {
-	result = &v1alpha2.PolicyReport{}
+func (c *policyReports) Patch(ctx context.Context, name string, pt types.PatchType, data []byte, opts v1.PatchOptions, subresources ...string) (result *PolicyReport, err error) {
+	result = &PolicyReport{}
 	err = c.client.Patch(pt).
 		Namespace(c.ns).
 		Resource("policyreports").
 		Name(name).
 		SubResource(subresources...).
-		SpecificallyVersionedParams(&opts, scheme.ParameterCodec, GroupVersion).
+		SpecificallyVersionedParams(&opts, ParameterCodec, GroupVersion).
 		Body(data).
 		Do(ctx).
 		Into(result)
